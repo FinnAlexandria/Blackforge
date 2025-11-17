3e2b435dd94232f3ee5f6fe582c7ac69420333c1
FinnAlexandria <zdvxvqdm54@privaterelay.appleid.com>
2025-11-17 20:41:00 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,16 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Blackforge — Luxury Edition",
   description: "Blackforge — Elite. Forged in gold.",
 };
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-bfBlack text-white antialiased">{children}</body>
+      <body className="bg-bfBlack text-white antialiased">
+        {children}
+      </body>
     </html>
   );
 }
