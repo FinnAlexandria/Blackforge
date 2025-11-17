c40cfe236afa289f0473d5e220df09b168fa6b2f
FinnAlexandria <zdvxvqdm54@privaterelay.appleid.com>
2025-11-17 19:30:07 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bfBlack: "#0b0b0d",
+        bfGold: "#d4af37",
+        bfGoldDark: "#a07f2a",
+        bfAccent: "#f3e5c6"
+      },
+      fontFamily: {
+        serif: ['Merriweather', 'serif'],
+        display: ['Georgia', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
